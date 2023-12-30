5345e62f2d23ab7b3b0a97aff8a83d44619b588c
Ankush <ankushrana830@gmail.com>
2023-12-31 02:58:53 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import {app} from './app.js';
  })
  connectDB()
  .then(() => {
-    app.listen(process.env.PORT || 3001, () => {
+    app.listen(process.env.PORT || 5000, () => {
         console.log(`⚙️  Server is running at port : ${process.env.PORT}`);
     })
 })
